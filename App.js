0788ca3d90b0bf864b27693ed76796be98e1d62b
weishenho <jijiwawaa@gmail.com>
2020-06-16 00:33:00 +0800

    init

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef, useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+
+
+const CountUp = ({num}) =>{
+  const aniVal = useRef(new Animated.Value(0)).current;
+  const [x, setX] = useState(0);
+
+
+
+  useEffect(() => {
+    aniVal.addListener((progress) => {
+
+      setX(progress.value.toFixed(0))
+    });
+
+    Animated.timing(aniVal, {
+      toValue: num,
+      duration: 2000,
+      easing:Easing.elastic(1.5),
+      useNativeDriver:true
+    }).start();
+  }, []);
+
+  return <Animated.Text>{x}</Animated.Text>
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CountUp num={100} />
     </View>
   );
 }
